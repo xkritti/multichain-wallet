5d2ecaef25348ae708b9818e42f289da6c2c64c5
born-sorravit <62236649+born-sorravit@users.noreply.github.com>
2022-08-26 00:39:40 +0700

    login

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
+export const api = () =>
+  axios.create({
+    baseURL: "http://localhost:4000",
+  });
+
+export const loginMember = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  const resp = await api().post("/singIn", { email, password });
+  return resp.data;
+};
 
 export default function handler(
   req: NextApiRequest,
